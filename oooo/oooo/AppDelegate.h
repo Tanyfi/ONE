4afa2fea9939a121edb8da93f17bf367d17b07d1
lanou <510187202@qq.com>
2016-08-01 14:41:49 +0800

    初始化项目

---
+++ oooo/oooo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  oooo
+//
+//  Created by lanou on 16/8/1.
+//  Copyright © 2016年 Tanyfi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
