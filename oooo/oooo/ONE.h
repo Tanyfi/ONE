9c6c436a6741fb5bd6774c86bb77cccf02ea1fc1
lanou <510187202@qq.com>
2016-08-01 14:44:58 +0800

    创建了one类

---
+++ oooo/oooo/ONE.h
@@ -0,0 +1,13 @@
+//
+//  ONE.h
+//  oooo
+//
+//  Created by lanou on 16/8/1.
+//  Copyright © 2016年 Tanyfi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ONE : NSObject
+
+@end
---
+++ oooo/oooo/ONE.m
@@ -0,0 +1,13 @@
+//
+//  ONE.m
+//  oooo
+//
+//  Created by lanou on 16/8/1.
+//  Copyright © 2016年 Tanyfi. All rights reserved.
+//
+
+#import "ONE.h"
+
+@implementation ONE
+
+@end
